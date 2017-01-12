162dae538ce4d373ff8bc760fe68b2b37b74abb1
Arun <adalurarun@users.noreply.github.com>
2017-01-12 14:54:05 +0530

    Update I2C device Scaner.lua

--- MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/I2C device Scaner.lua
+++ MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/I2C device Scaner.lua
@@ -4,19 +4,19 @@
 -- Arun
 
 -- setup
-id		=	0											--	always 0
-pinSDA	=	2											--	1~12, IO index
-pinSCL	=	1											--	1~12, IO index
-speed	=	i2c.SLOW									--	only i2c.SLOW supported
+id	=	0						--	always 0
+pinSDA	=	2						--	1~12, IO index
+pinSCL	=	1						--	1~12, IO index
+speed	=	i2c.SLOW					--	only i2c.SLOW supported
 
 --Initialize
-i2c.setup(id, pinSDA, pinSCL, speed)					--	Initialize the I²C module.
+i2c.setup(id, pinSDA, pinSCL, speed)				--	Initialize the I²C module.
 
 print("Scanning Started....")
 for count = 0,127 do
-  i2c.start(id)											--	Send an I²C start condition.
+  i2c.start(id)							-	Send an I²C start condition.
   Status = i2c.address(id, count, i2c.TRANSMITTER)		--	Setup I²C address and read/write mode for the next transfer.
-  i2c.stop(id)											--	Send an I²C stop condition.
+  i2c.stop(id)							--	Send an I²C stop condition.
 	if Status == true then
 		print("Addrss - "..count.." Detected device address is  0x" .. string.format("%02x", count) .. " (" .. count ..")") 
 	elseif Status == false then
