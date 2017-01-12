f6491e9e46ba9fab88d3701df55c7c7256503c1a
Arun <adalurarun@users.noreply.github.com>
2017-01-12 20:42:56 +0530

    Add files via upload

---
+++ MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/AEW_Relay Control By TCP_HTTP.lua
@@ -0,0 +1,12 @@
+-- Tested On Firmware and Details Below
+--[[
+	NodeMCU custom build by frightanic.com
+	branch: master
+	SSL: false
+	modules: bit,enduser_setup,file,gpio,http,i2c,mdns,mqtt,net,node,ow,pwm,rtcfifo,rtcmem,rtctime,sntp,tmr,uart,wifi
+ 	powered by Lua 5.1.4 on SDK 1.5.4.1(39cb9a32)
+]]--
+-- http://esp8266iot.blogspot.in/
+-- http://aruneworld.blogspot.com/
+-- Tested By 	: Arun (20170112)
+-- Example Name : AEW_Relay Control By TCP_HTTP.lua
