7a6116a0ac0551e6b4d7e91f37d8fd7c1131003a
Arun <adalurarun@users.noreply.github.com>
2017-02-13 14:26:27 +0530

    Add files via upload

---
+++ MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/AEW_DS18B20-Ow.lua
@@ -0,0 +1,65 @@
+
+--[[		Tested On Firmware and Details Below
+	NodeMCU custom build by frightanic.com
+	branch: master
+	SSL: false
+	modules: bit,enduser_setup,file,gpio,http,i2c,mdns,mqtt,net,node,ow,pwm,rtcfifo,rtcmem,rtctime,sntp,tmr,uart,wifi
+ 	powered by Lua 5.1.4 on SDK 1.5.4.1(39cb9a32)
+]]--
+-- http://esp8266iot.blogspot.in/
+-- http://aruneworld.blogspot.com/
+-- Tested By 	: Arun(20170112)
+-- Example Name : AEW_DS18B20-Ow.lua
+
+--data sheet : http://datasheets.maximintegrated.com/en/ds/DS18B20.pdf 
+
+pin = 1
+ow.setup(pin)
+count = 0
+repeat
+  count = count + 1
+  addr = ow.reset_search(pin)
+  addr = ow.search(pin)
+  tmr.wdclr()
+until (addr ~= nil) or (count > 100)
+if addr == nil then
+  print("No more addresses.")
+else
+  print(addr:byte(1,8))
+  crc = ow.crc8(string.sub(addr,1,7))
+  if crc == addr:byte(8) then
+    if (addr:byte(1) == 0x10) or (addr:byte(1) == 0x28) then
+      print("Device is a DS18S20 family device.")
+        repeat
+          ow.reset(pin)
+          ow.select(pin, addr)
+          ow.write(pin, 0x44, 1)
+          tmr.delay(1000000)
+          present = ow.reset(pin)
+          ow.select(pin, addr)
+          ow.write(pin,0xBE,1)
+   --       print("P="..present)  
+          data = nil
+          data = string.char(ow.read(pin))
+          for i = 1, 8 do
+            data = data .. string.char(ow.read(pin))
+          end
+--          print(data:byte(1,9))
+          crc = ow.crc8(string.sub(data,1,8))
+--          print("CRC="..crc)
+          if crc == data:byte(9) then
+             t = (data:byte(1) + data:byte(2) * 256) * 625
+             t1 = t / 10000
+             t2 = t % 10000
+--             print("Temperature="..t1.."."..t2.."Centigrade")
+			 print("Temperature : "..t1..". Centigrade")
+          end                   
+          tmr.wdclr()
+        until false
+    else
+      print("Device family is not recognized.")
+    end
+  else
+    print("CRC is not valid!")
+  end
+end
