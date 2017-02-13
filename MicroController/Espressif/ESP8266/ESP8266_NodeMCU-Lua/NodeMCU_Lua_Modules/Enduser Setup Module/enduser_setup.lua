fbd0700f39450de109ee2a6d4a4c23853691751a
Arun <adalurarun@users.noreply.github.com>
2017-02-13 16:33:22 +0530

    Delete enduser_setup.lua

--- MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules/Enduser Setup Module/enduser_setup.lua
+++
@@ -1,8 +0,0 @@
-enduser_setup.start(
-  function()
-    print("Connected to wifi as:" .. wifi.sta.getip())
-  end,
-  function(err, str)
-    print("enduser_setup: Err #" .. err .. ": " .. str)
-  end
-);
