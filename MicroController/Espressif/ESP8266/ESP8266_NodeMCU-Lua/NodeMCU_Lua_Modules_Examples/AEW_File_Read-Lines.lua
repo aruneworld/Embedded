ed168545b6a1b1bf427d21f515f7e30cd6807082
Arun <adalurarun@users.noreply.github.com>
2017-02-13 15:29:44 +0530

    Update AEW_File_Read-Lines.lua

--- MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/AEW_File_Read-Lines.lua
+++ MicroController/Espressif/ESP8266/ESP8266_NodeMCU-Lua/NodeMCU_Lua_Modules_Examples/AEW_File_Read-Lines.lua
@@ -11,9 +11,24 @@
 
 -- Example 	: AEW_File_Read-Lines.lua (this is the code of read the file lines )
 
-  file.open("Buzzer.lua")	-- File Name "Buzzer.lua"
+-----------Method-1------------
+  file.open("FileName")	-- File Name "Buzzer.lua"
   repeat
     local line=file.readline()
     if line then line=(string.gsub(line,"\n","")) print(line) end
   until not line
  file.close()
+
+---------Methode-2-------------
+if file.exists("FileName")	-- File Name "Buzzer.lua"
+	print("Initaialize time fileName file is exists")			
+	local _line 
+	if file.open("FileName","r") then print("--FileView start") repeat _line = file.readline()
+	if (_line~=nil) then print(string.sub(_line,1,-2)) end
+	until _line==nil
+	file.close() 
+	print("--FileView done.") 
+	end
+	else
+	print("Not have that fileS")
+end
