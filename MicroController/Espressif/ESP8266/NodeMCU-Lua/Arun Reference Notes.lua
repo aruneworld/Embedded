581e48e7fc02e4aceed42129e8f9a3a84ad2f75d
Arun <adalurarun@users.noreply.github.com>
2017-02-16 11:57:39 +0530

    Create Arun Reference Notes.lua

---
+++ MicroController/Espressif/ESP8266/NodeMCU-Lua/Arun Reference Notes.lua
@@ -0,0 +1,4 @@
+1. Find the solution of "Auto Detec Firmware"
+  "Can't autodetect firmware, because proper answer not received (may be unknown firmware). 
+  Please, reset module or continue.
+  ëëÕ½ou}¯­íë "
