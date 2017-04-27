62c328289e1fd209e7691339aa3a9a62f33db2c7
Arun <adalurarun@users.noreply.github.com>
2017-04-27 20:34:59 +0530

    Update AEW_GoogleTime.lua

--- MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW_GoogleTime.lua
+++ MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW_GoogleTime.lua
@@ -1,5 +1,33 @@
+--[[		Tested On Firmware and Details Below
+	NodeMCU custom build by frightanic.com
+	branch: master
+	SSL: false
+	modules: bit,enduser_setup,file,gpio,http,i2c,mdns,mqtt,net,node,ow,pwm,rtcfifo,rtcmem,rtctime,sntp,tmr,uart,wifi
+ 	powered by Lua 5.1.4 on SDK 1.5.4.1(39cb9a32)
+]]--
+-- http://esp8266iot.blogspot.in/
+-- http://aruneworld.blogspot.com/
+-- Tested By 	: Arun(20170219)
+-- Example Name : AEW_GoogleTime.lua
+
 station_cfg={}
-station_cfg.ssid= "JioFiUltimo" station_cfg.pwd= "8123020000UI"
+station_cfg.ssid= "Arun" station_cfg.pwd= "ArunEworld" -- Change your SSID and Password
+wifi.setmode(wifi.STATIONAP)
+wifi.sta.config(station_cfg)
+wifi.sta.connect()
+
+wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
+print("\n\tSTA - CONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..T.BSSID.."\n\tChannel: "..T.channel)
+
+end)
+wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
+	 print("\n\tSTA - DISCONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..T.BSSID.."\n\treason: "..T.reason)
+end)
+wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
+	 print("\n\tSTA - GOT IP".."\n\tStation IP: "..T.IP.."\n\tSubnet mask: "..T.netmask.."\n\tGateway IP: "..T.gateway)
+	 sta_ip = T.IP
+end)
+
 
 
 wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
@@ -34,3 +62,4 @@ conn:on("receive", function(conn, payload)
     end) 
 t = tmr.now()    
 conn:connect(80,'google.com')
+tmr.alarm(0,10000,1,function() conn:connect(80,'google.com') end) --every second Once
