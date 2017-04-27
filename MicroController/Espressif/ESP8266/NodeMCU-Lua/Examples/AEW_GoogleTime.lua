dfb0ba2336e385e051373696ae1a2c2016eb1af4
Arun <adalurarun@users.noreply.github.com>
2017-04-27 22:41:07 +0530

    Update AEW_GoogleTime.lua

--- MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW_GoogleTime.lua
+++ MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW_GoogleTime.lua
@@ -29,21 +29,6 @@ wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
 end)
 
 
-
-wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
-print("\n\tSTA - CONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..T.BSSID.."\n\tChannel: "..T.channel)
-
-end)
-wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, function(T)
-	 print("\n\tSTA - DISCONNECTED".."\n\tSSID: "..T.SSID.."\n\tBSSID: "..T.BSSID.."\n\treason: "..T.reason)
-end)
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
-	 print("\n\tSTA - GOT IP".."\n\tStation IP: "..T.IP.."\n\tSubnet mask: "..T.netmask.."\n\tGateway IP: "..T.gateway)
-	 sta_ip = T.IP
-end)
-
-
-
 conn=net.createConnection(net.TCP, 0) 
 
 conn:on("connection",function(conn, payload)
