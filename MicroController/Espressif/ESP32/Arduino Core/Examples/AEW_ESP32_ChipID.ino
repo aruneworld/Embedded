85f7af3631ede1039b10aeeb329438f1e4aadcb6
Arun <adalurarun@users.noreply.github.com>
2017-05-30 12:40:18 +0530

    Add files via upload

---
+++ MicroController/Espressif/ESP32/Arduino Core/Examples/AEW_ESP32_ChipID.ino
@@ -0,0 +1,34 @@
+uint64_t chipid;  
+
+void setup() {
+	Serial.begin(115200);
+}
+
+void loop() {
+	chipid=ESP.getEfuseMac();//The chip ID is essentially its MAC address(length: 6 bytes).
+	Serial.printf("ESP32 Chip ID = %04X",(uint16_t)(chipid>>32));//print High 2 bytes
+	Serial.printf("%08X\n",(uint32_t)chipid);//print Low 4bytes.
+
+	delay(3000);
+
+}
+
+/*
+ets Jun  8 2016 00:22:57
+
+rst:0x1 (POWERON_RESET),boot:0x13 (SPI_FAST_FLASH_BOOT)
+ets Jun  8 2016 00:22:57
+
+rst:0x10 (RTCWDT_RTC_RESET),boot:0x13 (SPI_FAST_FLASH_BOOT)
+configsip: 0, SPIWP:0x00
+clk_drv:0x00,q_drv:0x00,d_drv:0x00,cs0_drv:0x00,hd_drv:0x00,wp_drv:0x00
+mode:DIO, clock div:1
+load:0x3fff0008,len:8
+load:0x3fff0010,len:160
+load:0x40078000,len:10632
+load:0x40080000,len:252
+entry 0x40080034
+ESP32 Chip ID = EC5502A4AE30
+ESP32 Chip ID = EC5502A4AE30
+
+*/
