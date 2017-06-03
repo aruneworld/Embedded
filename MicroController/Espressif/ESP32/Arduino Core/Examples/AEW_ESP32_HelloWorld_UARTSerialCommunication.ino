56828596eb66c5aa35491735ec8031fa3d6ff646
Arun <adalurarun@users.noreply.github.com>
2017-06-03 18:38:57 +0530

    Add files via upload

---
+++ MicroController/Espressif/ESP32/Arduino Core/Examples/AEW_ESP32_HelloWorld_UARTSerialCommunication.ino
@@ -0,0 +1,18 @@
+/*
+	http://esp8266iot.blogspot.in/
+	http://aruneworld.blogspot.com/
+	Tested By 	: Arun(20170219)
+	Example Name : AEW_ESP32_HelloWorld_UARTSerialCommunication.ino
+*/
+
+void setup() {
+ Serial.begin(115200); // You can Change the Baudrate 
+}
+ 
+void loop() {
+ Serial.println("ArunEworld");
+ Serial.println("ESP32 Tutorials : Hello World");
+ Serial.println("[********************************]");
+ delay(2000); //2 sec delay
+ 
+}
