fae46c8e963c83bdd9947350b562f1f0108c266b
Arun <arun9047474939@gmail.com>
2018-04-06 12:14:45 +0530

    Create AEW Timer-WiFi IP Address Status Monitor.lua

---
+++ MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW Timer-WiFi IP Address Status Monitor.lua
@@ -0,0 +1,32 @@
+--www.ArunEworld.com/embedded/esp8266/esp8266-nodemcu/
+
+--Required NodeMCU Firmware Modules
+require("tmr")
+require("wifi")
+
+--WiFi Credentials Set
+SSID		=	"Arun"
+PASSWORD	=	"8300026060"
+
+--Timer parameters Set
+TIMER_ID = 0
+TIMER_DELAY_DURATION = 1000		--1second
+TIMER_MODE = 1		--Repeatably
+
+--WiFi Configuration
+wifi.sta.config(SSID, PASSWORD)
+
+--Local WiFi IP Address Monitor User Defined Function with No Arguments
+local function WiFi_IP_ADDRESS_STATUS_MONITOR()
+	--Check the IP address i if loop using Wifi.sta.getip() function
+	if wifi.sta.getip()==nil then
+      print("connecting to AP...") 
+   else
+      print('ip: ',wifi.sta.getip())
+      tmr.stop(0)						-- Stop the timer
+   end
+
+end
+
+--timer alarm function 
+tmr.alarm(TIMER_ID, TIMER_DELAY_DURATION, TIMER_MODE, WiFi_IP_ADDRESS_STATUS_MONITOR()	end)
