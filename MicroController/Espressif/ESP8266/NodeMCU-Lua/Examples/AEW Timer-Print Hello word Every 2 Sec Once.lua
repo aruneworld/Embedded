460d4a7ca70bc946d0d1b82054b3bd8e8d503463
Arun <arun9047474939@gmail.com>
2018-04-07 03:26:01 +0530

    Update AEW Timer-Print Hello word Every 2 Sec Once.lua

--- MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW Timer-Print Hello word Every 2 Sec Once.lua
+++ MicroController/Espressif/ESP8266/NodeMCU-Lua/Examples/AEW Timer-Print Hello word Every 2 Sec Once.lua
@@ -1,9 +1,12 @@
 --www.ArunEworld.com/embedded/esp8266/esp8266-nodemcu/
 
+--Required NodeMCU Firmware Modules 
+require("tmr")
+
 --Timer parameters Set
 TIMER_ID = 0
 TIMER_DELAY_DURATION = 2000		--2second
 TIMER_MODE = 1		--Repeatably
 
 --timer alarm function 
-tmr.alarm(TIMER_ID, TIMER_DELAY_DURATION, TIMER_MODE, function() Print("Hello World")	end)
+tmr.alarm(TIMER_ID, TIMER_DELAY_DURATION, TIMER_MODE, function() print("Hello World")	end)
